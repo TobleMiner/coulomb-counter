6f37763571cdd1fb3b9097c4165cfffddffae6e7
Tobias Schramm <tobleminer@gmail.com>
2019-03-25 04:33:08 +0100

    TWI_I2C: Fix I2C statemachine

--- lipoCcounter/twi_i2c_slave.c
+++ lipoCcounter/twi_i2c_slave.c
@@ -1,8 +1,12 @@
 #include "twi_i2c_slave.h"
 #include "util.h"
 #include "time.h"
+#include "debug.h"
+
+#define F_CPU 8000000UL
 
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
 #define I2C_STATUS_START_W 0x60
 #define I2C_STATUS_START_R 0xA8
@@ -18,29 +22,17 @@ enum {
 	I2C_STATE_DATA
 };
 
-enum {
-	I2C_MODE_R = 0,
-	I2C_MODE_W
-};
-
-static struct {
+struct {
+	uint8_t state:2;
 	struct timeval_t timeout;
 	struct timeval_t last_xfer;
-	uint8_t mode:1;
-	uint8_t state:2;
 	struct TWI_I2C_Reg** regs;
-	uint8_t num_regs;
 	struct TWI_I2C_Reg* active_reg;
-// 	uint8_t reg_addr;
-// 	uint8_t num_bytes;
-// 	uint8_t buff[I2C_BUFF_SIZE];
-// 	uint8_t* buff_r;
-// 	uint8_t* buff_w;
+	uint8_t num_regs;
 } i2c;
 
 void TWI_I2C_Init(char address, struct TWI_I2C_Reg** regs, uint8_t num_regs) {
 	i2c.state = I2C_STATE_IDLE;
-	i2c.mode = I2C_MODE_R;
 	i2c.timeout.secs = 0;
 	i2c.timeout.nsecs = 10000000UL;
 	i2c.last_xfer.nsecs = 0;
@@ -52,10 +44,12 @@ void TWI_I2C_Init(char address, struct TWI_I2C_Reg** regs, uint8_t num_regs) {
 	TWAR = (address << 1);
 }
 
+uint8_t i2c_state;
+
 uint8_t TWI_I2C_Busy() {
 	struct timeval_t time;
 	struct timeval_t delta;
-	if(!i2c.state == I2C_STATE_IDLE) {
+	if(i2c.state == I2C_STATE_IDLE) {
 		return 0;
 	}
 	now_fast(&time);
@@ -65,75 +59,60 @@ uint8_t TWI_I2C_Busy() {
 		i2c.state = I2C_STATE_IDLE;
 	}
 	ATOMIC_END
-	return i2c.state == I2C_STATE_IDLE;;
+	return i2c.state != I2C_STATE_IDLE;
 }
 
-ISR(TWI_vect)
-{
+ISR(TWI_vect) {
 	uint8_t status = TWSR & 0b11111000;
-	switch(i2c.mode)
+	switch(status)
 	{
-		case I2C_MODE_R:
-			switch(status)
-			{
-				case I2C_STATUS_START_W:
-					i2c.state = I2C_STATE_ADDRESS;
-					i2c.active_reg = NULL;
-					break;
-				case I2C_STATUS_BYTE_R:
+		case I2C_STATUS_START_W:
+			i2c.state = I2C_STATE_ADDRESS;
+			i2c.active_reg = NULL;
+			break;
+		case I2C_STATUS_BYTE_R:
 //					i2c.num_bytes++;
-					switch(i2c.state) {
-						case I2C_STATE_ADDRESS:
-							if(TWDR < i2c.num_regs) {
-								i2c.active_reg = i2c.regs[TWDR];
-							} else {
-								i2c.active_reg = NULL;
-							}
-							i2c.state = I2C_STATE_DATA;
-							break;
-						case I2C_STATE_DATA:
-							if(i2c.active_reg) {
-								i2c.active_reg->data = TWDR;
-								i2c.active_reg->attr.changed = 1;
-							}
-							break;
-						default:
-							i2c.state = I2C_STATE_IDLE;
+			switch(i2c.state) {
+				case I2C_STATE_ADDRESS:
+					status = TWDR;
+					if(status < i2c.num_regs) {
+						i2c.active_reg = i2c.regs[status];
+					} else {
+						i2c.active_reg = NULL;
+					}
+					i2c.state = I2C_STATE_DATA;
+					break;
+				case I2C_STATE_DATA:
+					if(i2c.active_reg) {
+						i2c.active_reg->data = TWDR;
+						i2c.active_reg->attr.changed = 1;
 					}
 					break;
-				case I2C_STATUS_STOP_W:
-// Just ognore number of bytes written for now
-//					if(i2c.num_bytes == 1) {
-						i2c.mode = I2C_MODE_W;
-//					}
-					break;
-				default: // Reset I2C
-					TWCR |= (1<<TWEA) | (1<<TWSTO);
-					i2c.active_reg = NULL;
+				default:
 					i2c.state = I2C_STATE_IDLE;
 			}
 			break;
-		case I2C_MODE_W:
-			switch(status)
-			{
-				case I2C_STATUS_START_R:
-					i2c.state = I2C_STATE_DATA;
-				case I2C_STATUS_BYTE_W:
-					if(i2c.active_reg) {
-						TWCR = i2c.active_reg->data;
-					} else {
-						TWCR = 0;
-					}
-					break;
-				case I2C_STATUS_NAK_W:
-				case I2C_STATUS_STOP_R:
-				default:
-					TWCR |= (1<<TWEA) | (1<<TWSTO);
-					i2c.mode = I2C_MODE_R;
-					i2c.state = I2C_STATE_IDLE;
-					i2c.active_reg = NULL;
+		case I2C_STATUS_STOP_W:
+// Just ignore number of bytes written for now
+//					if(i2c.num_bytes == 1) {
+//					}
+			break;
+		case I2C_STATUS_START_R:
+			i2c.state = I2C_STATE_DATA;
+		case I2C_STATUS_BYTE_W:
+			if(i2c.active_reg) {
+				TWDR = i2c.active_reg->data;
+			} else {
+				TWDR = 0;
 			}
+			break;
+		case I2C_STATUS_NAK_W:
+		case I2C_STATUS_STOP_R:
+		default:
+			TWCR |= BIT(TWEA) | BIT(TWSTO);
+			i2c.state = I2C_STATE_IDLE;
+			i2c.active_reg = NULL;
 	}
-	TWCR |= (1<<TWINT);
 	now_fast(&i2c.last_xfer);
+	TWCR |= BIT(TWINT);
 }
